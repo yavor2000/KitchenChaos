51bec530743b6ef4d6b3625783853159e021685a
yavor2000 <yavor2000@yahoo.com>
2023-03-24 04:24:15 +0200

    KC001 Player animations added. Cinemachine added.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Vector2 _inputVector;
+    private bool _isWalking;
+
+    public bool IsWalking
+    {
+        get => _isWalking;
+        private set => _isWalking = value;
+    }
+
+    [SerializeField]
+    private float moveSpeed = 7f;
+    [SerializeField]
+    private float rotateSpeed = 10f;
+    
+    private void Awake()
+    {
+        _inputVector = new Vector2(0, 0);
+    }
+
+    private void Update()
+    {
+        _inputVector = new Vector2(0, 0);
+        
+        if (Input.GetKey(KeyCode.W))
+        {
+            _inputVector.y = 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            _inputVector.y = -1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            _inputVector.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            _inputVector.x = 1;
+        }
+
+        _inputVector = _inputVector.normalized;
+        Vector3 moveDir = new Vector3(_inputVector.x, 0f, _inputVector.y);
+        _isWalking = moveDir != Vector3.zero;
+        
+        var ptransform = transform;
+        ptransform.position += moveDir * (moveSpeed * Time.deltaTime);
+        ptransform.forward = Vector3.Slerp(ptransform.forward, moveDir, rotateSpeed * Time.deltaTime);
+    }
+
+}
---
+++ Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    private Animator animator;
+    private static readonly int IsWalking = Animator.StringToHash("IsWalking");
+
+    [SerializeField] private Player player;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        animator.SetBool(IsWalking, player.IsWalking);
+    }
+}
