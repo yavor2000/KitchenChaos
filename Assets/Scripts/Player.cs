83821c5ecb73b65fa2d5f997c10ddcf0179fd4ac
yavor2000 <yavor2000@yahoo.com>
2023-03-24 06:20:11 +0200

    KC001 Collision Detection

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Player : MonoBehaviour
 {
@@ -14,12 +15,12 @@ public class Player : MonoBehaviour
         private set => _isWalking = value;
     }
 
-    [SerializeField]
-    private float moveSpeed = 7f;
-    [SerializeField]
-    private float rotateSpeed = 10f;
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float rotateSpeed = 10f;
+    [SerializeField] private float playerRadius = .65f;
+    [SerializeField] private float playerHeight = 2f;
 
-    [SerializeField] private GameInput _gameInput;
+    [SerializeField] private GameInput gameInput;
     
     private void Awake()
     {
@@ -28,12 +29,42 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        _inputVector = _gameInput.GetMovementVectorNormalized();
+        _inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(_inputVector.x, 0f, _inputVector.y);
         _isWalking = moveDir != Vector3.zero;
         
         var ptransform = transform;
-        ptransform.position += moveDir * (moveSpeed * Time.deltaTime);
+        Vector3 ppos = ptransform.position;
+        var moveDistance = moveSpeed * Time.deltaTime;
+        bool canMove = !Physics.CapsuleCast(ppos, ppos + Vector3.up * playerHeight,
+            playerRadius, moveDir, moveDistance);
+        if (!canMove)
+        {
+            // Attempt only X movement
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0); //.normalized();
+            canMove = !Physics.CapsuleCast(ppos, ppos + Vector3.up * playerHeight,
+                playerRadius, moveDirX, moveDistance);
+            if (canMove)
+            {
+                moveDir = moveDirX;
+            }
+            else
+            {
+                // Attempt only Z movement
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z); //.normalized();
+                canMove = !Physics.CapsuleCast(ppos, ppos + Vector3.up * playerHeight,
+                    playerRadius, moveDirZ, moveDistance);
+                if (canMove)
+                {
+                    moveDir = moveDirZ;
+                } // else can't move in any direction                
+            }
+        }
+        if (canMove)
+        {
+            ptransform.position += moveDir * moveDistance;
+        }
+        
         ptransform.forward = Vector3.Slerp(ptransform.forward, moveDir, rotateSpeed * Time.deltaTime);
     }
 
