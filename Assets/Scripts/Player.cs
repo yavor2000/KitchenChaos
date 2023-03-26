8172720aa664d620ae2719a19d133b67be2625a6
yavor2000 <yavor2000@yahoo.com>
2023-03-27 01:05:15 +0300

    KC001 Clear Counter

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,4 @@
-using System;
+ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +8,7 @@ public class Player : MonoBehaviour
 {
     private Vector2 _inputVector;
     private bool _isWalking;
+    private Vector3 _lastInteractionDir;
 
     public bool IsWalking
     {
@@ -21,6 +22,8 @@ public class Player : MonoBehaviour
     [SerializeField] private float playerHeight = 2f;
 
     [SerializeField] private GameInput gameInput;
+    [SerializeField] private LayerMask countersLayerMask;
+    
     
     private void Awake()
     {
@@ -29,19 +32,34 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        HandleMovement();
+        HandleInteractions();
+    }
+
+    private void HandleMovement()
+    {
         _inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(_inputVector.x, 0f, _inputVector.y);
         _isWalking = moveDir != Vector3.zero;
-        
+
         var ptransform = transform;
         Vector3 ppos = ptransform.position;
         var moveDistance = moveSpeed * Time.deltaTime;
         bool canMove = !Physics.CapsuleCast(ppos, ppos + Vector3.up * playerHeight,
-            playerRadius, moveDir, moveDistance);
+            playerRadius, moveDir, out RaycastHit raycastHit, moveDistance);
+        // if (!canMove)
+        // {
+        //     Debug.Log($"deb: moveDir {moveDir}, raycastHit {raycastHit.normal}");
+        //     moveDir += raycastHit.normal;
+        //     // moveDir += new Vector3(raycastHit.normal.x * .75f, 0, raycastHit.normal.z * 0.75f);
+        //     Debug.Log($"deb: newDir  {moveDir}");
+        //     ppos += moveDir * moveDistance;
+        // }
+
         if (!canMove)
         {
             // Attempt only X movement
-            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0); //.normalized();
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0); //.normalized;
             canMove = !Physics.CapsuleCast(ppos, ppos + Vector3.up * playerHeight,
                 playerRadius, moveDirX, moveDistance);
             if (canMove)
@@ -51,7 +69,7 @@ public class Player : MonoBehaviour
             else
             {
                 // Attempt only Z movement
-                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z); //.normalized();
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z); //.normalized;
                 canMove = !Physics.CapsuleCast(ppos, ppos + Vector3.up * playerHeight,
                     playerRadius, moveDirZ, moveDistance);
                 if (canMove)
@@ -60,12 +78,36 @@ public class Player : MonoBehaviour
                 } // else can't move in any direction                
             }
         }
+        
         if (canMove)
         {
             ptransform.position += moveDir * moveDistance;
         }
-        
+
         ptransform.forward = Vector3.Slerp(ptransform.forward, moveDir, rotateSpeed * Time.deltaTime);
     }
-
+    
+    private void HandleInteractions()
+    {
+        _inputVector = gameInput.GetMovementVectorNormalized();
+        Vector3 moveDir = new Vector3(_inputVector.x, 0f, _inputVector.y);
+        if (moveDir != Vector3.zero)
+        {
+            _lastInteractionDir = moveDir;
+        }
+        
+        float interactionDistance = 2f;
+        if (Physics.Raycast(transform.position, _lastInteractionDir, out RaycastHit raycastHit,
+                interactionDistance, countersLayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                // Has ClearCounter
+                clearCounter.Interact();
+            }
+            
+            
+            
+        }
+    }
 }
