e645439b06d35788614d992f6bf4a23caa84e0d0
yavor2000 <yavor2000@yahoo.com>
2023-03-28 08:02:50 +0300

    KC001 Kitchen Object, ScriptableObjects

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -1,11 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class ClearCounter : MonoBehaviour
 {
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+    [SerializeField] private Transform counterTopPoint;
+    
     public void Interact()
     {
         Debug.Log("Interact!");
+        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+        kitchenObjectTransform.localPosition = Vector3.zero;
+
+        KitchenObjectSO kso = kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO();
+        Debug.Log($"Spawn {kso.objectName}");
     }
 }
---
+++ Assets/Scripts/KitchenObject.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class KitchenObject : MonoBehaviour
+{
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+
+    public KitchenObjectSO GetKitchenObjectSO()
+    {
+        return kitchenObjectSO;
+    }
+}
