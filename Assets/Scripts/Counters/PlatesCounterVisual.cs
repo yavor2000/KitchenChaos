41644feaf233d191c29c9983583bc4b06b693c84
yavor2000 <yavor2000@yahoo.com>
2023-04-05 14:32:49 +0300

    KC001 Index out of range fix.

--- Assets/Scripts/Counters/PlatesCounterVisual.cs
+++ Assets/Scripts/Counters/PlatesCounterVisual.cs
@@ -12,7 +12,6 @@ namespace Counters
         [SerializeField] private Transform plateVisualPrefab;
 
         private List<GameObject> _plateVisualGameObjectList;
-        private int _platesCount = 0;
         private const float PlateSizeY = 0.1f;
 
         private void Awake()
@@ -28,8 +27,9 @@ namespace Counters
 
         private void PlatesCounter_OnPlateRemoved(object sender, EventArgs e)
         {
-            GameObject go = _plateVisualGameObjectList.ElementAt(_platesCount - 1);
-            _plateVisualGameObjectList.RemoveAt(_platesCount - 1);
+            int plateVisualGameObjectListCount = _plateVisualGameObjectList.Count;
+            GameObject go = _plateVisualGameObjectList.ElementAt(plateVisualGameObjectListCount - 1);
+            _plateVisualGameObjectList.RemoveAt(plateVisualGameObjectListCount - 1);
             Destroy(go);
         }
 
@@ -39,9 +39,8 @@ namespace Counters
             // Vector3 newPos = plateVisualTransform.position;
             // newPos.y += _platesCount * 0.2f;
             // plateVisualTransform.position = newPos;
-            plateVisualTransform.localPosition = new Vector3(0, _platesCount * PlateSizeY, 0);
+            plateVisualTransform.localPosition = new Vector3(0, _plateVisualGameObjectList.Count * PlateSizeY, 0);
             _plateVisualGameObjectList.Add(plateVisualTransform.gameObject);
-            _platesCount = _plateVisualGameObjectList.Count;
         }
     }
 }
