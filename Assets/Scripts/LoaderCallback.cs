20272a9771765de796bcb4bd639c5355f58a2c4c
yavor2000 <yavor2000@yahoo.com>
2023-04-13 14:51:40 +0300

    KC001 Main Menu, Loading

---
+++ Assets/Scripts/LoaderCallback.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoaderCallback : MonoBehaviour
+{
+    private bool _isFirstUpdate = true;
+
+    private void Update()
+    {
+        if (_isFirstUpdate)
+        {
+            _isFirstUpdate = false;
+            Loader.LoaderCallback();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace UI
+{
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button quitButton;
+
+
+    private void Awake()
+    {
+        playButton.onClick.AddListener(() =>
+        {
+            Loader.Load(Loader.Scene.GameScene);
+        });
+        
+        quitButton.onClick.AddListener(() =>
+        {
+            Application.Quit();
+        });
+    }
+}
+}
